9893b11cd2eaf53b199fe24e1c7d6536bc62c0e3
Catalina Turlea <catalina.turlea@gmail.com>
2020-02-08 13:05:43 +0100

    Initial commit

---
+++ precache-manifest.a2145e69cb18c7b8413e3cbb4fc35533.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ff2eab2966c53531fb0c925b6083d70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d082dd4445f60c873fa9",
+    "url": "/static/css/2.65b39531.chunk.css"
+  },
+  {
+    "revision": "6a3afb9cdf5d96380d2b",
+    "url": "/static/css/main.c6b21c6e.chunk.css"
+  },
+  {
+    "revision": "d082dd4445f60c873fa9",
+    "url": "/static/js/2.d53be546.chunk.js"
+  },
+  {
+    "revision": "6916fd35d3e0362216fcf374aa575f3c",
+    "url": "/static/js/2.d53be546.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6a3afb9cdf5d96380d2b",
+    "url": "/static/js/main.27a2e21a.chunk.js"
+  },
+  {
+    "revision": "696942293a92ac3a2bb6",
+    "url": "/static/js/runtime-main.889ad4b6.js"
+  }
+]);
