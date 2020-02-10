8d690daccd925b51f6074dd3e2f2fe4a8565d5c0
Catalina Turlea <catalina.turlea@gmail.com>
2020-02-10 17:50:48 +0100

    Update

---
+++ precache-manifest.013d8b38b5eae3ecfc94058c47e64c22.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee9219689132681b551d6a3dd9ee9f0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8183dbb83530d7fa648d",
+    "url": "/static/css/2.65b39531.chunk.css"
+  },
+  {
+    "revision": "8659e12735c0d75fd206",
+    "url": "/static/css/main.51ab9990.chunk.css"
+  },
+  {
+    "revision": "8183dbb83530d7fa648d",
+    "url": "/static/js/2.ec4ad8b4.chunk.js"
+  },
+  {
+    "revision": "717c077b76ea816bccb313eff48fa635",
+    "url": "/static/js/2.ec4ad8b4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8659e12735c0d75fd206",
+    "url": "/static/js/main.e80c254b.chunk.js"
+  },
+  {
+    "revision": "696942293a92ac3a2bb6",
+    "url": "/static/js/runtime-main.889ad4b6.js"
+  }
+]);
